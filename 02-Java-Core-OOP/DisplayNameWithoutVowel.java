b95eac60d5300087c6cd7e2df26276d72711c753
manas8173 <manasgupta1011@gmail.com>
2026-06-16 09:20:57 +0530

    commit

---
+++ 02-Java-Core-OOP/DisplayNameWithoutVowel.java
@@ -0,0 +1,5 @@
+public class DisplayNameWithoutVowel {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ 02-Java-Core-OOP/NameWithoutSemicollin.java
@@ -0,0 +1,5 @@
+public class NameWithoutSemicollin {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
